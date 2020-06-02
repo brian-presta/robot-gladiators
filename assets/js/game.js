44e06f50cff1ce1bfb04d145f26c6eec212e6579
brian presta <brian.presta@gmail.comgit commit -m first>
2020-06-02 13:11:41 -0700

    put opponents in array, set up for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,31 +1,51 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto","Amy Android","Robo Trumble"];
+console.log(enemyNames)
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
   window.alert("Welcome to Robot Gladiators!");
-  enemyHealth -= playerAttack;
-  console.log("Roborto's health is "+ enemyHealth);
-  if (enemyHealth < 1){
-      window.alert(enemyName + " has died!")
-  }
-  else {
-      window.alert(enemyName + " has " + enemyHealth + " health remaining")
-  }
-  playerHealth -= enemyAttack;
-  if (playerHealth < 1){
-    window.alert(playerName + " has died!")
-}
-else {
-    window.alert(playerName + " has " + playerHealth + " health remaining")
-}
-  console.log(playerName+"'s health is "+playerHealth)
+  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle?");
+  if (promptFight == 'fight' || promptFight == "FIGHT") {
+    enemyHealth -= playerAttack;
+    if (enemyHealth < 1){
+        window.alert(enemyName + " has died!")
+    }
+    else {
+        window.alert(enemyName + " has " + enemyHealth + " health remaining")
+    }
+    playerHealth -= enemyAttack;
+    if (playerHealth < 1){
+        window.alert(playerName + " has died!")
+    }
+    else {
+        window.alert(playerName + " has " + playerHealth + " health remaining")
+    }
+    console.log(playerName+"'s health is "+playerHealth)
+    }
+    else if (promptFight == "skip" || promptFight == "SKIP") {
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?")
+        if (confirmSkip ){
+        window.alert(playerName + " has chosen to skip the fight!")
+        playerMoney -= 2
+        console.log("The player has " + playerMoney + " dollars.")
+        }
+        else {
+            fight()
+        }
+    } else {
+        window.alert("You need to pick a valid option. Try again!")
+    }
 };
 
-fight();
+for(x in enemyNames){
+    enemyName = enemyNames[x]
+    fight(enemyName)}
+
