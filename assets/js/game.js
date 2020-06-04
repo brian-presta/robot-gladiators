793d3bef820585a0eab5b1b9527b994a43df9403
brian presta <brian.presta@gmail.comgit commit -m first>
2020-06-03 17:54:38 -0700

    fixed blank name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -92,8 +92,14 @@ var shop = function() {
 
 };
 // player info
+var getPlayerName = function() {
+    var name = ""
+    while (name === "" || name === null) {
+        name = window.prompt("What is your robot's name?");
+    }
+}
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
